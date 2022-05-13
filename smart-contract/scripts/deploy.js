f98aacf254cad3f235a592b3cef1f895506105c0
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-05-13 22:01:09 +0100

    Added all configuration

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -3,18 +3,22 @@ const { ethers } = require('hardhat');
 const main = async () => {
   const dogeFactory = await ethers.getContractFactory('Dogecoin');
   const dogeContract = await dogeFactory.deploy();
+  await dogeContract.deployed();
   console.log('Dogecoin deployed at: ', dogeContract.address);
 
   const bitcoinFactory = await ethers.getContractFactory('Btc');
   const bitcoinContract = await bitcoinFactory.deploy();
+  await bitcoinContract.deployed();
   console.log('bitcoin deployed at: ', bitcoinContract.address);
 
   const solanaFactory = await ethers.getContractFactory('Solana');
   const solanaContract = await solanaFactory.deploy();
+  await solanaContract.deployed();
   console.log('Solana deployed at: ', solanaContract.address);
 
   const usdcFactory = await ethers.getContractFactory('Usdc');
   const usdcContract = await usdcFactory.deploy();
+  await usdcContract.deployed();
   console.log('USDC deployed at: ', usdcContract.address);
 };
 
